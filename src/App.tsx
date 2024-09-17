508a6fff0b884de250c5b1b38f51abe63b70731f
Josephine Closan <89244648+JoeClos@users.noreply.github.com>
2024-09-17 18:05:26 +0300

    change render burger menu on all routes except landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,40 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
-import { useState } from "react";
+
+// Custom hook to get current location
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const location = useLocation();
+  
+  const showNavbar = location.pathname !== '/';
+
+  return (
+    <>
+      {showNavbar && <Navbar />}
+      <main className={`main-content`}>
+        {children}
+      </main>
+    </>
+  );
+}
 
 function App() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [isNavbarOpen] = useState<boolean>(false);
 
   return (
     <Router>
-      <Navbar />
-      <main className={`main-content ${isNavbarOpen ? 'shifted' : ''}`}>
+      <Layout>
         <Routes>
-          <Route path="/" element={<Home />}>Home</Route>
-          <Route path="/about" element={<About />}>About</Route>
-          <Route path="/portfolio" element={<Portfolio />}>Portfolio</Route>
-          <Route path="/contact" element={<Contact />}>Contact</Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
-      </main>
+      </Layout>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
